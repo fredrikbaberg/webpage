e9726e1a3c1f16ad7d66d319de72cf24be837711
Fredrik Baberg <fredrik.baberg@gmail.com>
2020-02-12 12:29:51 +0100

    Export hugo path in before_install

--- build.sh
+++ build.sh
@@ -2,16 +2,12 @@
 BURL=https://fredrikbaberg.github.io
 cd sites/
 cd start/
-echo "$(pwd)"
-$HOME/bin/hugo --baseURL=$BURL
+hugo --baseURL=$BURL
 cd ../bio
-echo "$(pwd)"
-$HOME/hugo --baseURL=$BURL/bio
+hugo --baseURL=$BURL/bio
 cd ../presentation
-echo "$(pwd)"
-$HOME/hugo --baseURL=$BURL/presentation
+hugo --baseURL=$BURL/presentation
 cd ../scouter
-echo "$(pwd)"
-$HOME/hugo --baseURL=$BURL/scouter
+hugo --baseURL=$BURL/scouter
 cd ../../
 echo "Klart att kopiera public/ till server."
