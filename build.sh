033d979075b79927178bb30182d26a1e0ad126c5
Fredrik Baberg <fredrik.baberg@gmail.com>
2020-02-12 11:15:11 +0100

    Updated again

--- build.sh
+++ build.sh
@@ -2,4 +2,15 @@
 BURL=https://fredrikbaberg.github.io
 cd sites/
 cd start/
+echo "$(pwd)"
 hugo --baseURL=$BURL
+cd ../bio
+echo "$(pwd)"
+hugo --baseURL=$BURL/bio
+cd ../presentation
+echo "$(pwd)"
+hugo --baseURL=$BURL/presentation
+cd ../scouter
+echo "$(pwd)"
+hugo --baseURL=$BURL/scouter
+cd ../../
